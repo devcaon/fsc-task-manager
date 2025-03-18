8f46c04656eb30b06e589faf86d606fb791b8994
Claudio OLiveira <dev.caon@gmail.com>
2025-03-18 20:54:13 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,20 +1,42 @@
+import CheckIcon from "../assets/icons/check.svg?react";
+import LoaderIcon from "../assets/icons/loader.svg?react";
+import DatailsIcon from "../assets/icons/details.svg?react";
+
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
-    if (task.status === "Pending") {
-      return "bg-[#35383E]/10 text-[#35383E]";
+    if (task.status === "pending") {
+      return "bg-[#35383E] bg-opacity-10 text-[#35383E]";
     }
-    if (task.status === "In progress") {
-      return "bg-[#FFAA04]/10 text-[#FFAA04]";
+    if (task.status === "in_progress") {
+      return "bg-[#FFAA04] text-[#FFAA04]";
     }
-    if (task.status === "Done") {
-      return "bg-[#00ADB5]/10 text-[#00ADB5]";
+    if (task.status === "done") {
+      return "bg-[#00ADB5] text-[#00ADB5]";
     }
   };
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-spin text-[#FFFFFF]" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="transition hover:opacity-70">
+        <DatailsIcon />
+      </a>
     </div>
   );
 };
--- src/constants/tasks.js
+++ src/constants/tasks.js
@@ -4,21 +4,21 @@ const TASKS = [
     title: "Estudar React",
     description: "Estudar React para melhorar minhas habilidades",
     time: "Morning",
-    status: "In progress",
+    status: "in_progress",
   },
   {
     id: 2,
     title: "Estudar TailwindCSS",
     description: "Estudar TailwindCSS para melhorar minhas habilidades",
     time: "Afternoon",
-    status: "Pending",
+    status: "pending",
   },
   {
     id: 3,
     title: "Estudar Next.js",
     description: "Estudar Next.js para melhorar minhas habilidades",
     time: "Evening",
-    status: "Done",
+    status: "done",
   },
   {
     id: 4,
@@ -26,28 +26,28 @@ const TASKS = [
     description:
       "Estudar Git para possibilitar trabalho remoto em equipe de desenvolvimento",
     time: "Morning",
-    status: "Pending",
+    status: "in_progress",
   },
   {
     id: 5,
     title: "Ir para acadêmia",
     description: "Fazer exercícios físicos para manter a saúde em dia",
     time: "Evening",
-    status: "Pending",
+    status: "pending",
   },
   {
     id: 6,
     title: "Rezar o Rosário",
     description: "Rezar o Rosário para manter a fé em dia",
     time: "Morning",
-    status: "Pending",
+    status: "pending",
   },
   {
     id: 7,
     title: "Trabalhar no projeto",
     description: "Trabalhar no projeto para entregar no prazo",
     time: "Morning",
-    status: "Done",
+    status: "done",
   },
 ];
 
